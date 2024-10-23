5e78dec8612a970c7a55df93a8d70612d258ccdc
nwamaka-o <Nuokalla@student.rtc.edu>
2024-10-23 06:11:29 +0000

    Added python file

---
+++ main.py
@@ -0,0 +1,32 @@
+def row_row_row_your_boat():
+    print("Row, row, row your boat,")
+    print("Gently down the stream.")
+    
+def life_is_a_dream():
+    print("Merrily, merrily, merrily, merrily,")
+    print("Life is but a dream.")
+
+def first_verse():
+    row_row_row_your_boat()
+    life_is_a_dream()
+
+def second_verse():
+    print("Row, row, row your boat,")
+    print("Gently down the stream.")
+    print("If you see a crocodile,")
+    print("Don't forget to scream!")
+
+def third_verse():
+    print("Row, row, row your boat,")
+    print("Gently down the shore.")
+    print("If you see a lion there,")
+    print("Don't forget to roar!")
+
+def play_song():
+    first_verse()
+    second_verse()
+    third_verse()
+
+# Test the code by calling play_song
+if __name__ == "__main__":
+    play_song()
